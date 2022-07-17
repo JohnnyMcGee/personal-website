a802e6568efcb7011eef0d8d1449802610a56258
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-07-17 11:29:25 -0500

    Add contact form submit endpoint

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+const { initializeApp } = require("firebase/app");
 const express = require("express");
 const path = require("path");
+// const { body, validationResult } = require("express-validator");
+var bodyParser = require("body-parser");
 
 const app = express();
 const port = 80;
 
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
+// app.use(express.json()); // to support JSON-encoded bodies
+// app.use(express.urlencoded()); // to support URL-encoded bodies
+
 app.use("/", express.static(path.join(__dirname, "public")));
+
+const firebaseInit = () => {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD_F99wD_6rErCvfQEtOXwZOligaQAvIq0",
+    authDomain: "johnny-mcgee-website.firebaseapp.com",
+    projectId: "johnny-mcgee-website",
+    storageBucket: "johnny-mcgee-website.appspot.com",
+    messagingSenderId: "991571377236",
+    appId: "1:991571377236:web:0bfe30093b747408f141c1",
+  };
+
+  // Initialize Firebase
+  firebaseInit();
+  const app = initializeApp(firebaseConfig);
+};
+
+// validate input and store the form data
 app.post("/contact-submit", (req, res) => {
+  console.log(req.body);
   res.send("Hello World!");
 });
 
