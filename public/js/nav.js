919cb67df5d3a5ed5e13676b71fdf7d7e04499e2
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-07-19 01:40:57 -0500

    Add background to contact screen

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,21 +1,7 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
+var lastScrollTop; // This Varibale will store the top position
 
-window.addEventListener('scroll', () => { 
-  if (prevScrollpos < window)
-});
+navbar = document.getElementById("nav"); // Get The NavBar
 
-// window.onscroll = function onScrollNav() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("nav").style.top = "0";
-//   } else {background: rgb(0, 0, 0);
-    background: linear-gradient( 20deg, rgba(0, 0, 0, 0.5) 0%, rgb(62 104 126 / 50%) 100% );
-//     document.getElementById("nav").style.top = "-100px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// };
-
-// document.body.addEventListener("scroll", () => {
-//   console.log(document.documentElement.scrollTop || document.body.scrollTop);
-// }); 
+document.getElementsByTagName('body')[0].onscroll = () => {
+    console.log("scrolling");
+};
